cdcd3401bebf325bc2f828f4c2c7650ad951a6f2
Yb-rabbit <1715610143@qq.com>
2025-03-22 21:03:10 +0800

    合并
    检查与修改

--- Assets/Script/Rabbit/GridSystem/ObjectPlacer.cs
+++ Assets/Script/Rabbit/GridSystem/ObjectPlacer.cs
@@ -4,11 +4,20 @@ public class ObjectPlacer : MonoBehaviour
 {
     public GridManager gridManager; // 引用 GridManager
     public GameObject objectPrefab; // 要放置的物体 Prefab
+    public int maxObjectCount = 10; // 最大生成数量
+
+    private int currentObjectCount = 0; // 当前已生成的物体数量
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // 按下鼠标左键
         {
+            if (currentObjectCount >= maxObjectCount)
+            {
+                Debug.Log("已达到最大生成数量");
+                return; // 如果已达到最大生成数量，则不再生成新的物体
+            }
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) // 检测鼠标点击位置
@@ -19,6 +28,7 @@ public class ObjectPlacer : MonoBehaviour
                     var cellIndex = gridManager.GetCellIndexByWorldPosition(hit.point);
                     // 在该格子上放置物体
                     gridManager.PlaceObjectAtCell(Instantiate(objectPrefab), cellIndex.Item1, cellIndex.Item2);
+                    currentObjectCount++; // 增加已生成的物体数量
                 }
             }
         }
